d356073fea59905ad676775f9538d1aebdddd420
Ab-mahin <mahin.cse7.bu@gmail.com>
2024-07-03 02:18:11 +0600

    Create icpc-dhaka-21-J-.cpp

---
+++ icpc-dhaka-21-J-.cpp
@@ -0,0 +1,71 @@
+// USER: "Ab.Mahin"
+// Problem: ICPC Standing
+// Contest: LightOJ
+// URL: https://lightoj.com/problem/icpc-standing
+// Memory Limit: 128 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+//#define m 1000000007
+
+
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Findout buggs:
+
+#define BUG
+
+#ifdef BUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+cout << name << " : " << arg1 << '\n';
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');
+cout.write(names, comma - names) << " : " << arg1<< " | ";__f(comma+1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+//...................
+const ll N = 2e5 + 5;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif*/
+
+    ll t, tt;
+    t = tt = 1;
+    
+    cin >> t;
+	
+    while(t--){
+        ll n, s, r;
+        cin >> n >> s >> r;
+        
+        ll ok = 1;
+        if(n == s && r != 1) ok = 0;
+        cout << "Case " << tt++ << ": " << (ok ? "Yes" : "No") << '\n';
+    }
+    
+    
+    return 0;
+}
